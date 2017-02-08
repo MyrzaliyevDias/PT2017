3d67e2ddbd105917ede7330931dc2f49fd9767b7
MyrzaliyevDias <myrzaliev.kz11@gmail.com>
2017-02-09 03:08:40 +0600

    Add files via upload

---
+++ laborat_1_2/laborat_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laborat_1_2
+{
+    class Programm
+    {
+        static void Main()
+        {
+            student a, b, c;
+            a = new student(4.0, "Myrzaliev", "Dias", 18); // используем конструктор для определения
+            b = new student(4.0, "Borash", "Kazbek", 18);
+            c = new student(4.0, "Seilova", "Zaure", 18);
+            Console.WriteLine(a + "\n" + b + "\n" + c); // выводим данные студентов
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ laborat_1_2/laborat_1_2/student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laborat_1_2
+{
+    class student
+    {
+        // Храним 4 значения для каждого студента
+        // их средний балл, имя, фамилия и возраст
+        double gpa;
+        string name, surname;
+        int age;
+        //конструктор принимающий значения
+        public student(double _gpa, string family_name, string first_name, int _age)
+        {
+            gpa = _gpa;
+            name = first_name;
+            surname = family_name;
+            age = _age;
+        }
+        //     |переписанный| метод
+        public   override       string ToString()
+        {
+            //форматированный вывод данных студента
+            return "Student: " + name + " " + surname + " at the age of " + age + " has GPA " + gpa;
+        }
+    }
+
+}
