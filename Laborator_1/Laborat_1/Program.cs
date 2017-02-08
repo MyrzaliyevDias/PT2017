b616f0db79883c2a147e9737b4b3c5aa71a46e69
MyrzaliyevDias <myrzaliev.kz11@gmail.com>
2017-02-09 02:33:49 +0600

    Add files via upload

---
+++ Laborator_1/Laborat_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborat_1
+{
+    class Program
+    {
+        // Проверка на простоту числа
+       static bool check(string number)
+        {
+            // Перевод в int
+            int x = Convert.ToInt32(number);
+            int y = 0; // Количество делителей
+            for (int i = 1; i <= x; i++)
+                if (x % i == 0)
+                    y++; //Если делится то это один из делителей
+            if (y == 2) return true;    //Если у числа всего два делителя - то это простое число
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < args.Length; i++) //Пробегаемся по числам
+            {
+                if (check(args[i]))
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+            Console.ReadKey(); //Остановка программы
+        }
+
+    }
+}
