3ecde6d685f1d7f47f3bf029ab6ddca3c0827adb
MyrzaliyevDias <myrzaliev.kz11@gmail.com>
2017-03-09 12:22:15 +0600

    Add files via upload

---
+++ Mid1/Mid1/Mid1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mid1
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            for (int i = 1; i <= n; ++i)
+            {
+                if (n % i == 0)
+                    Console.Write(i + " ");
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
