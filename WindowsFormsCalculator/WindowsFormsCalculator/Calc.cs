1e7080662a89b80c2b0cf5cd274384ea7dde1399
MyrzaliyevDias <myrzaliev.kz11@gmail.com>
2017-03-22 13:43:13 +0600

    Calculator

---
+++ WindowsFormsCalculator/WindowsFormsCalculator/Calc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCalculator
+{
+    class Calc
+    {
+        public double firstnumber;
+        public string operation;
+        public double result;
+        public double secondnumber;
+
+        public void calculate()
+        {
+            switch (operation)//Рассматриваем случай для каждой операции
+            {
+                case "+"://Сам случай если операция сложения
+                    result = firstnumber + secondnumber;
+                    break;
+                case "-"://Сам случай если операция вычитание
+                    result = firstnumber - secondnumber;
+                    break;
+                case "*":
+                    result = firstnumber * secondnumber;
+                    break;
+                case "/":
+                    result = firstnumber / secondnumber;
+                    break;
+                case "%":
+                    result = firstnumber / 100 * secondnumber;
+                    break;
+                case "x^2":
+                    result = firstnumber * firstnumber;
+                    break;
+                case "1/x":
+                    result = 1 / firstnumber;
+                    break;
+                case "Sqrt":
+                    result = Math.Sqrt(firstnumber);
+                    break;
+            }
+
+        }
+    }
+   
+}
---
+++ WindowsFormsCalculator/WindowsFormsCalculator/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCalculator
+{
+    public partial class Form1 : Form
+    {
+        Calc calc = new Calc();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numbers_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;//Button выполняет работает с кнопкой на которую мы нажимаем
+            Display.Text += btn.Text;
+       }
+        private void operation_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;//Button выполняет работает с кнопкой на которую мы нажимаем
+            calc.firstnumber = double.Parse(Display.Text);
+            calc.operation = btn.Text;  
+            Display.Text = "";
+        }
+        private void result_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;//Button выполняет работает с кнопкой на которую мы нажимаем
+           if(calc.operation !="Sqrt" && calc.operation != "1/X" && calc.operation != "x^2")
+            {
+                calc.secondnumber = double.Parse(Display.Text);
+                calc.calculate();
+                Display.Text = calc.result.ToString();
+            }
+            else
+            {
+                calc.calculate();
+                Display.Text = calc.result.ToString();
+            }
+        }
+        private void C_click(object sender,EventArgs e)
+        {
+            Display.Text = "";
+            Calc calc = new Calc();
+        }
+        private void CE_click(object sender,EventArgs e)
+        {
+            Display.Text = "";
+            Calc calc = new Calc();
+        }
+
+        private void Backspace_Click(object sender, EventArgs e)
+        {
+            if (Display.Text != "")
+                Display.Text = Display.Text.Remove(Display.Text.Length - 1);
+
+        }
+    
+      
+    }
+}
